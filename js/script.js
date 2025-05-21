3d68259d45b22066d46485964bda8cd66fbd7919
aldanblsss <aldaatahir94@gmail.com>
2025-05-22 00:53:25 +0800

    menambahkan payment

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 // toggle class active untuk hamburger menu
-const NavbarNav = document.querySelector(".navbar-nav");
+const NavbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  NavbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = () => {
+  NavbarNav.classList.toggle('active');
 };
 
 // Toggle class active untuk search form
-const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-document.querySelector("#search-button").onclick = (e) => {
-  searchForm.classList.toggle("active");
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
   searchBox.focus();
   e.preventDefault();
 };
 
 // Toggle class active untuk shopping cart
-const shoppingCart = document.querySelector(".shopping-cart");
-document.querySelector("#shoppingg-cart-button").onclick = (e) => {
-  shoppingCart.classList.toggle("active");
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
   e.preventDefault();
 };
 
 //klik di luar elemen
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
-const sc = document.querySelector("#shoppingg-cart-button");
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
-document.addEventListener("click", function (e) {
+document.addEventListener('click', function (e) {
   if (!hm.contains(e.target) && !NavbarNav.contains(e.target)) {
-    NavbarNav.classList.remove("active");
+    NavbarNav.classList.remove('active');
   }
 
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove("active");
+    searchForm.classList.remove('active');
   }
   if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove("active");
+    shoppingCart.classList.remove('active');
   }
 });
